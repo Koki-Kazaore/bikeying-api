bb108016676909720bb70d1ea563c967890c5e2e
Koki-Kazaore <kazaore@icloud.com>
2024-11-06 14:02:45 +0900

    (6.13)Override flask-smorest's dynamically generated API specification

--- dispatch/app.py
+++ dispatch/app.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import yaml
+from apispec import APISpec
+
 from flask import Flask
 from flask_smorest import Api
 
@@ -17,3 +21,12 @@ dispatch_api = Api(app)
 
 # Register the blueprint with the Dispatch API object
 dispatch_api.register_blueprint(blueprint)
+
+api_spec = yaml.safe_load((Path(__file__).parent / "oas.yaml").read_text())
+spec = APISpec(
+    title=api_spec["info"]["title"],
+    version=api_spec["info"]["version"],
+    openapi_version=api_spec["openapi"],
+)
+spec.to_dict = lambda: api_spec
+dispatch_api.spec = spec
