00419de6a6f1c7a0ca41f62eaa585ec826b1171d
Koki-Kazaore <kazaore@icloud.com>
2024-11-08 17:30:46 +0900

    Integrate API specification

--- dispatch/api/api.py
+++ dispatch/api/api.py
@@ -1,19 +1,24 @@
 import uuid
-from datetime import datetime
 
-from flask.views import MethodView
-from flask_smorest import Blueprint
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from fastapi import HTTPException
+from starlette.responses import Response
+from starlette import status
+
+from app import app
+# from flask.views import MethodView
+# from flask_smorest import Blueprint
 
 # Import marshmallow models
-from api.schemas import (
+from schemas import (
   DispatchRequestSchema,
   DispatchBikeSchema,
   DispatchedBikeSchema,
   GetDispatchedBikeSchema
 )
-
-# Create an instance of the Blueprint class
-blueprint = Blueprint('dispatch', __name__, description='Dispatch API')
 
 # Declare a list of hard-coded assignments results
 results = [
@@ -27,14 +32,9 @@ results = [
   },
 ]
 
-# Using Blueprint's response() decorator,
-# Register a marshmallow model for the response pyaload
-@blueprint.response(status_code=200, schema=GetDispatchedBikeSchema)
-# Implement an URL path as function-based view
-@blueprint.route('/bikes/dispatch', methods=['POST'])
-@blueprint.arguments(DispatchBikeSchema)
-def dispatch_bike(dispath_request):
+@app.post("/bikes/dispatch", response_model=GetDispatchedBikeSchema)
+def dispatch_bike(dispath_request: DispatchBikeSchema):
     """
     Dispatch a bike to a user
     """
-    return results
+    return {"results": results}
